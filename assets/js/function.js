e95f96f72195d0883048ed58ebaf745ffbf6266b
mahibulmasud <mahibulmasud@gmail.com>
2022-07-08 19:09:29 +0600

    edited

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,11 +1,24 @@
 // humberger start
 function handleHumberger() {
     let menu = document.getElementsByClassName("menu_ul");
+    let l1 = document.getElementById("line1");
+    let l2 = document.getElementById("line2");
+    let l3 = document.getElementById("line3");
 
     if (menu[0].style.left === "-1000px") {
         menu[0].style.left = "0px";
+        l1.style.marginBottom = "-8px"
+        l1.style.transform = "rotate(45deg)"
+        l2.style.visibility = "hidden"
+        l3.style.marginTop = "-8px"
+        l3.style.transform = "rotate(-45deg)"
     } else {
         menu[0].style.left = "-1000px"
+        l1.style.marginBottom = "0px"
+        l1.style.transform = "rotate(0deg)"
+        l2.style.visibility = "visible"
+        l3.style.marginTop = "0px"
+        l3.style.transform = "rotate(0deg)"
     }
 }
 // humberger end
